91476cff47c7517c51b72a8b42f6bb2fa0754776
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-04-29 18:05:19 -0300

    adicionando constantes

---
+++ lib/constantes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//todas as cores que serão usadas no app
+Color kCorDeTelaDeFundo = const Color(0xFF5C8ACF);
+Color kCorBotao = const Color(0xFFCE58B4);
+Color kCorNomeDeMenosDestaque = const Color(0xFFDEDEDE);
+Color kCorNomeDaTelaIncial = const Color(0xFFFFFFFF);
+Color kCorDaCaixa = Color(0xFFFAFAFA);
+
+//imagens
+const String SLOGAN = "assets/images/sloga.png";
