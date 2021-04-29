47729a66342583a94b1c6120bbc980ae8199ef7d
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-04-29 18:22:39 -0300

    atualizando main

--- lib/main.dart
+++ lib/main.dart
@@ -19,88 +19,91 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset(
-            SLOGAN,
-          ),
-          Text(
-            "Dogs & Cats",
-            style: TextStyle(
-              color: kCorNomeDaTelaIncial,
-              fontWeight: FontWeight.bold,
-              fontSize: 36,
+      body: Container(
+        padding: EdgeInsets.only(top: 30, bottom: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              SLOGAN,
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 12, top: 20, bottom: 20),
-            width: 305,
-            height: 68,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: kCorDaCaixa,
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Nome",
-                labelStyle: TextStyle(
-                  fontSize: 24,
-                ),
-                enabledBorder: InputBorder.none,
+            Text(
+              "Dogs & Cats",
+              style: TextStyle(
+                color: kCorNomeDaTelaIncial,
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 12, top: 20, bottom: 20),
-            width: 305,
-            height: 68,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: kCorDaCaixa,
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Senha",
-                labelStyle: TextStyle(
-                  fontSize: 24,
-                ),
-                enabledBorder: InputBorder.none,
+            Container(
+              padding: EdgeInsets.only(left: 12, top: 20, bottom: 20),
+              width: 305,
+              height: 68,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: kCorDaCaixa,
               ),
-            ),
-          ),
-          Container(
-            width: 283,
-            height: 59,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "ENTRAR",
-                style: TextStyle(
-                  color: kCorNomeDaTelaIncial,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Nome",
+                  labelStyle: TextStyle(
+                    fontSize: 24,
+                  ),
+                  enabledBorder: InputBorder.none,
                 ),
               ),
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 12, top: 20, bottom: 20),
+              width: 305,
+              height: 68,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: kCorDaCaixa,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  labelStyle: TextStyle(
+                    fontSize: 24,
+                  ),
+                  enabledBorder: InputBorder.none,
+                ),
+              ),
+            ),
+            Container(
+              width: 283,
+              height: 59,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "ENTRAR",
+                  style: TextStyle(
+                    color: kCorNomeDaTelaIncial,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
                   ),
                 ),
-                backgroundColor: MaterialStateProperty.all(kCorBotao),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  backgroundColor: MaterialStateProperty.all(kCorBotao),
+                ),
               ),
             ),
-          ),
-          Text(
-            "Esqueci a senha",
-            style: TextStyle(
-              color: kCorNomeDeMenosDestaque,
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
+            Text(
+              "Esqueci a senha",
+              style: TextStyle(
+                color: kCorNomeDeMenosDestaque,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
