7e040d6757c7bdc8c744fb76c5180f5d3d4ecdc3
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-05-19 16:15:30 -0300

    atualizando campo de texto

--- lib/models/caixaTexto.dart
+++ lib/models/caixaTexto.dart
@@ -4,30 +4,44 @@ class CaixaTexto extends StatelessWidget {
   final double? largura, altura, tamanhoFonte;
   final Color? corCaixa;
   final String? nome;
+  final bool aparecerOuNao;
   CaixaTexto({
     @required this.largura,
     @required this.altura,
     @required this.corCaixa,
     @required this.nome,
     @required this.tamanhoFonte,
+    this.aparecerOuNao = false,
   });
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(left: 12, top: 20, bottom: 20),
+      margin: EdgeInsets.only(left: 50, right: 50),
       width: largura,
       height: altura,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: corCaixa,
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.white,
       ),
-      child: TextField(
-        decoration: InputDecoration(
-          labelText: nome,
-          labelStyle: TextStyle(
-            fontSize: tamanhoFonte,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: TextField(
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.grey,
+            ),
+            decoration: InputDecoration(
+              hintText: nome,
+              hintStyle: TextStyle(
+                fontSize: tamanhoFonte,
+              ),
+              enabledBorder: InputBorder.none,
+              border: InputBorder.none,
+            ),
+            obscuringCharacter: "*",
+            obscureText: aparecerOuNao,
           ),
-          enabledBorder: InputBorder.none,
         ),
       ),
     );
