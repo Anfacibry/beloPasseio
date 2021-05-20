6798f04fd4b9cd0683a3cc3ec6a7337d53f4d838
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-05-20 00:20:14 -0300

    atualizando tudo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:belopasseio/constantes.dart';
-import 'package:belopasseio/pages/login.dart';
+//import 'package:belopasseio/pages/login.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
@@ -12,7 +12,158 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: kCorDeTelaDeFundo,
       ),
-      home: Login(),
+      home: HomePrincipal(),
+    );
+  }
+}
+
+class HomePrincipal extends StatefulWidget {
+  @override
+  _HomePrincipalState createState() => _HomePrincipalState();
+}
+
+class _HomePrincipalState extends State<HomePrincipal> {
+  ImageProvider imageProvider = AssetImage(PERFIL1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Bem vindo de volta",
+                        style: TextStyle(
+                          color: kCorNomeDeMenosDestaque,
+                          fontSize: 15,
+                        ),
+                      ),
+                      Text(
+                        "Fabricio Cintra",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  CircleAvatar(
+                    backgroundImage: imageProvider,
+                    maxRadius: 30,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 200,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 170,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                              FUNDO,
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: Stack(
+                          children: [
+                            Center(child: Image.asset(DOG2)),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "2 com frio",
+                              style: TextStyle(
+                                fontSize: 15,
+                              ),
+                            ),
+                            Text(
+                              "Bob",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text("Passeio"),
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                              backgroundColor:
+                                  MaterialStateProperty.all(kCorBotao)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  clipBehavior: Clip.antiAlias,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Convidar para um passeio",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Criar",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/pages/login.dart
@@ -0,0 +1,102 @@
+import 'package:belopasseio/models/caixaTexto.dart';
+import 'package:flutter/material.dart';
+import 'package:belopasseio/constantes.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double altura = alturaPega(context);
+    double largura = larguraPega(context);
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.only(top: altura * 0.04, bottom: altura * 0.01),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  SLOGAN,
+                ),
+                SizedBox(
+                  height: altura * 0.04,
+                ),
+                Text(
+                  "Dogs & Cats",
+                  style: TextStyle(
+                    color: kCorNomeDaTelaIncial,
+                    fontWeight: FontWeight.bold,
+                    fontSize: largura * 0.12,
+                  ),
+                ),
+                SizedBox(
+                  height: altura * 0.04,
+                ),
+                //Caixa de Texto de login
+                CaixaTexto(
+                  largura: largura * 0.8,
+                  altura: (largura * 0.8) - (largura * 0.65),
+                  corCaixa: kCorDaCaixa,
+                  nome: "Nome",
+                  tamanhoFonte: largura * 0.09,
+                  aparecerOuNao: false,
+                ),
+                SizedBox(
+                  height: altura * 0.02,
+                ),
+                //Caixa de Texto de senha
+                CaixaTexto(
+                  largura: largura * 0.8,
+                  altura: (largura * 0.8) - (largura * 0.65),
+                  corCaixa: kCorDaCaixa,
+                  nome: "Senha",
+                  tamanhoFonte: largura * 0.09,
+                  aparecerOuNao: true,
+                ),
+                SizedBox(
+                  height: altura * 0.02,
+                ),
+                //Container com apresentação do botão para entrar com login e senha
+                Container(
+                  width: largura * 0.6,
+                  height: (largura * 0.8) - (largura * 0.65),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "ENTRAR",
+                      style: TextStyle(
+                        color: kCorNomeDaTelaIncial,
+                        fontWeight: FontWeight.bold,
+                        fontSize: largura * 0.08,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      backgroundColor: MaterialStateProperty.all(kCorBotao),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: altura * 0.01,
+                ),
+                //Botão de redefinir a senha
+                Text(
+                  "Esqueci a senha",
+                  style: TextStyle(
+                    color: kCorNomeDeMenosDestaque,
+                    fontWeight: FontWeight.bold,
+                    fontSize: largura * 0.06,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
