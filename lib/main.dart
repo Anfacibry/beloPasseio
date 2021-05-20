e3314756ce4dd547bd59374c61d4170ce3ee8a98
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-05-19 23:00:18 -0300

    atualizando main

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         scaffoldBackgroundColor: kCorDeTelaDeFundo,
       ),
@@ -19,91 +20,95 @@ class MyApp extends StatelessWidget {
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double altura = alturaPega(context);
+    double largura = larguraPega(context);
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.only(top: 30, bottom: 30),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                SLOGAN,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Dogs & Cats",
-                style: TextStyle(
-                  color: kCorNomeDaTelaIncial,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 36,
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.only(top: altura * 0.04, bottom: altura * 0.01),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  SLOGAN,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              //Caixa de Texto de login
-              CaixaTexto(
-                largura: 305,
-                altura: 60,
-                corCaixa: kCorDaCaixa,
-                nome: "Nome",
-                tamanhoFonte: 24,
-                aparecerOuNao: false,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              //Caixa de Texto de senha
-              CaixaTexto(
-                largura: 305,
-                altura: 60,
-                corCaixa: kCorDaCaixa,
-                nome: "Senha",
-                tamanhoFonte: 24,
-                aparecerOuNao: true,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              //Container com apresentação do botão para entrar com login e senha
-              Container(
-                width: 283,
-                height: 59,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "ENTRAR",
-                    style: TextStyle(
-                      color: kCorNomeDaTelaIncial,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
+                SizedBox(
+                  height: altura * 0.04,
+                ),
+                Text(
+                  "Dogs & Cats",
+                  style: TextStyle(
+                    color: kCorNomeDaTelaIncial,
+                    fontWeight: FontWeight.bold,
+                    fontSize: largura * 0.12,
                   ),
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+                ),
+                SizedBox(
+                  height: altura * 0.04,
+                ),
+                //Caixa de Texto de login
+                CaixaTexto(
+                  largura: largura * 0.8,
+                  altura: (largura * 0.8) - (largura * 0.65),
+                  corCaixa: kCorDaCaixa,
+                  nome: "Nome",
+                  tamanhoFonte: largura * 0.09,
+                  aparecerOuNao: false,
+                ),
+                SizedBox(
+                  height: altura * 0.02,
+                ),
+                //Caixa de Texto de senha
+                CaixaTexto(
+                  largura: largura * 0.8,
+                  altura: (largura * 0.8) - (largura * 0.65),
+                  corCaixa: kCorDaCaixa,
+                  nome: "Senha",
+                  tamanhoFonte: largura * 0.09,
+                  aparecerOuNao: true,
+                ),
+                SizedBox(
+                  height: altura * 0.02,
+                ),
+                //Container com apresentação do botão para entrar com login e senha
+                Container(
+                  width: largura * 0.6,
+                  height: (largura * 0.8) - (largura * 0.65),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "ENTRAR",
+                      style: TextStyle(
+                        color: kCorNomeDaTelaIncial,
+                        fontWeight: FontWeight.bold,
+                        fontSize: largura * 0.08,
                       ),
                     ),
-                    backgroundColor: MaterialStateProperty.all(kCorBotao),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      backgroundColor: MaterialStateProperty.all(kCorBotao),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              //Botão de redefinir a senha
-              Text(
-                "Esqueci a senha",
-                style: TextStyle(
-                  color: kCorNomeDeMenosDestaque,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+                SizedBox(
+                  height: altura * 0.01,
                 ),
-              ),
-            ],
+                //Botão de redefinir a senha
+                Text(
+                  "Esqueci a senha",
+                  style: TextStyle(
+                    color: kCorNomeDeMenosDestaque,
+                    fontWeight: FontWeight.bold,
+                    fontSize: largura * 0.06,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
