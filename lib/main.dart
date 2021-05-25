e0956ab4099cb29ec048d12359cd42d68fb4084e
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-05-24 23:20:42 -0300

    atualizando main para testes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:belopasseio/constantes.dart';
+import 'package:belopasseio/models/dogsCats.dart';
 //import 'package:belopasseio/pages/login.dart';
 import 'package:flutter/material.dart';
 
@@ -28,120 +29,73 @@ class _HomePrincipalState extends State<HomePrincipal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 60),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Bem vindo de volta",
-                        style: TextStyle(
-                          color: kCorNomeDeMenosDestaque,
-                          fontSize: 15,
-                        ),
-                      ),
-                      Text(
-                        "Fabricio Cintra",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  CircleAvatar(
-                    backgroundImage: imageProvider,
-                    maxRadius: 30,
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20, top: 60, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  height: 200,
-                  width: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 100,
-                        width: 170,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(
-                              FUNDO,
-                            ),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: Stack(
-                          children: [
-                            Center(child: Image.asset(DOG2)),
-                          ],
-                        ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Bem vindo de volta",
+                      style: TextStyle(
+                        color: kCorNomeDeMenosDestaque,
+                        fontSize: 15,
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "2 com frio",
-                              style: TextStyle(
-                                fontSize: 15,
-                              ),
-                            ),
-                            Text(
-                              "Bob",
-                              style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
+                    ),
+                    Text(
+                      "Fabricio Cintra",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Center(
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Text("Passeio"),
-                          style: ButtonStyle(
-                              shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                              ),
-                              backgroundColor:
-                                  MaterialStateProperty.all(kCorBotao)),
-                        ),
-                      ),
-                    ],
-                  ),
-                  clipBehavior: Clip.antiAlias,
+                    ),
+                  ],
+                ),
+                CircleAvatar(
+                  backgroundImage: imageProvider,
+                  maxRadius: 30,
                 ),
               ],
             ),
-            SizedBox(
-              height: 20,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                ...ANIMAIS.map((e) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DogsCats(
+                      FUNDO,
+                      e["imagem"]!,
+                      e["status"]!,
+                      e["nome"]!,
+                      alturaPrincipal: 200,
+                      larguraPrincipal: 150,
+                      alturaSecundaria: 100,
+                      larguraSecundaria: 150,
+                      corBotao: kCorBotao,
+                    ),
+                  );
+                }).toList(),
+              ],
             ),
-            Row(
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
@@ -160,9 +114,9 @@ class _HomePrincipalState extends State<HomePrincipal> {
                   ),
                 ),
               ],
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
