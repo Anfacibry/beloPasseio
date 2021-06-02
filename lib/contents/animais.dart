aa527c07e0fb58e3b64416aea4ab411e43f5225a
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-06-02 14:46:04 -0300

    atualizando codigo

---
+++ lib/contents/animais.dart
@@ -0,0 +1,20 @@
+import '../constantes.dart';
+
+//Lista de animais cadastrados
+const List<Map<String, String>> ANIMAIS = [
+  {
+    "status": "2 com frio",
+    "nome": "Bob",
+    "imagem": DOG1,
+  },
+  {
+    "status": "1 com sede",
+    "nome": "Chocolate",
+    "imagem": DOG2,
+  },
+  {
+    "status": "3 com fome",
+    "nome": "Esponja",
+    "imagem": CAT1,
+  },
+];
