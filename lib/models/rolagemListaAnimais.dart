bd673a06db849c8060762594fae82ee3cb4c87c5
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-06-03 00:06:29 -0300

    add rolagemListaAnimais

---
+++ lib/models/rolagemListaAnimais.dart
@@ -0,0 +1,36 @@
+import 'package:belopasseio/contents/animais.dart';
+import 'package:flutter/material.dart';
+
+import '../constantes.dart';
+import 'dogsCats.dart';
+
+class RolageListaAnimais extends StatelessWidget {
+  const RolageListaAnimais({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          ...ANIMAIS.map((e) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DogsCats(
+                FUNDO,
+                e["imagem"]!,
+                e["status"]!,
+                e["nome"]!,
+                alturaPrincipal: 200,
+                larguraPrincipal: 150,
+                alturaSecundaria: 100,
+                larguraSecundaria: 150,
+                corBotao: kCorBotao,
+              ),
+            );
+          }).toList(),
+        ],
+      ),
+    );
+  }
+}
