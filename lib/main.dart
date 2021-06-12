66db3abc5376aec22c2c667ac02581b7ac997943
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-06-11 23:14:03 -0300

    atualizando main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:belopasseio/constantes.dart';
+import 'package:belopasseio/pages/home.dart';
 import 'package:belopasseio/pages/login.dart';
-//import 'package:belopasseio/pages/login.dart';
+import 'package:belopasseio/routes/rotas.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(MyApp());
@@ -9,13 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: kCorDeTelaDeFundo,
-        fontFamily: FontSignikaNegativeFamily,
-        accentColor: kCorBotao,
-      ),
-      home: Login(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: kCorDeTelaDeFundo,
+          fontFamily: FontSignikaNegativeFamily,
+          accentColor: kCorBotao,
+        ),
+        initialRoute: AppRoutes.ROTA_PRINCIPAL,
+        routes: {
+          AppRoutes.ROTA_PRINCIPAL: (context) => Login(),
+          AppRoutes.ROTA_HOME: (context) => HomePrincipal(),
+        });
   }
 }
