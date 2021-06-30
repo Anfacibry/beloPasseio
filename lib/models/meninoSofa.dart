060aa9f7e5692be5fdea82bb8e84e61f53ddae39
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-06-30 16:30:17 -0300

    atualizando meninoSofa

--- lib/models/meninoSofa.dart
+++ lib/models/meninoSofa.dart
@@ -7,109 +7,86 @@ class MeninoSofa extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      //Início do Stack que faz todo o layut com o menino no sofá e o menino
-      // com o cachorro
+    return Column(
       children: [
-        Column(
-          children: [
-            Container(
-              height: 100,
-              width: 400,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(20),
-                  topLeft: Radius.circular(20),
-                ),
-                color: Colors.white,
-                image: DecorationImage(
-                  image: AssetImage(
-                    FUNDO,
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  Positioned(
-                    top: -36,
-                    child: Container(
-                      width: 350,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Image.asset(
-                            MENINOSOFA,
-                            height: 160,
-                            width: 160,
-                          ),
-                          Image.asset(
-                            MENINO2,
-                            height: 150,
-                            width: 150,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+        Container(
+          height: 100,
+          width: 400,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20),
+              topLeft: Radius.circular(20),
             ),
-            Container(
-              height: 70,
-              width: 400,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(20),
-                  bottomLeft: Radius.circular(20),
-                ),
-                color: Colors.white,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Gabriel e Junior",
-                          style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          "25 de maio - 2021",
-                        ),
-                      ],
-                    ),
+            color: Colors.white,
+          ),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Positioned(
+                top: -22,
+                child: Container(
+                  width: 392,
+                  child: Image.asset(
+                    MENINOSOFA2,
+                    fit: BoxFit.cover,
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Rota",
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 70,
+          width: 400,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(20),
+              bottomLeft: Radius.circular(20),
+            ),
+            color: Colors.white,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Gabriel e Junior",
                       style: TextStyle(
+                        fontSize: 25,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        kCorBotao,
-                      ),
-                      shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
+                    Text(
+                      "25 de maio - 2021",
                     ),
-                  )
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Rota",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    kCorBotao,
+                  ),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ],
     );
