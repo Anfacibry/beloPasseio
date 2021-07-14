ff2bf205ff5a5e9b337678485040cafb12895ebc
Anfacibry <anfacibry@gmail.com>
2021-07-14 09:49:19 -0300

    atualizando modelos/meninoSofá para responsividade

--- lib/models/meninoSofa.dart
+++ lib/models/meninoSofa.dart
@@ -1,3 +1,4 @@
+import 'package:belopasseio/models/mediaQuery.dart';
 import 'package:flutter/material.dart';
 
 import '../constantes.dart';
@@ -7,37 +8,29 @@ class MeninoSofa extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double largura = AlturaLargura().larguraPega(context);
     return Column(
       children: [
-        Container(
-          height: 100,
-          width: 400,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(20),
-              topLeft: Radius.circular(20),
-            ),
-            color: Colors.white,
-          ),
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Positioned(
-                top: -22,
-                child: Container(
-                  width: 400,
-                  child: Image.asset(
-                    MENINOSOFA2,
-                    fit: BoxFit.cover,
-                  ),
-                ),
+        Padding(
+          padding: const EdgeInsets.only(top: 2),
+          child: Container(
+            height: largura * 0.30,
+            width: largura * 1,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20),
+                topLeft: Radius.circular(20),
               ),
-            ],
+              image: DecorationImage(
+                image: AssetImage(MENINOSOFA2),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
         ),
         Container(
           height: 70,
-          width: 400,
+          width: largura * 1,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.only(
               bottomRight: Radius.circular(20),
